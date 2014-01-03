8e5275d2344a9177332273ef2285134c65d777bd
unknown <soumyagh@Insoumyagh.corporate.thoughtworks.com>
2014-01-03 16:56:54 +0530

    HelloWorld Java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+
+// alias h="rm HelloWorld.class; javac HelloWorld.java && java HelloWorld"
